e9170642f27348ed606afedc0e3b927bb845b516
Karthik Balasubramanian <143219880+karthik-bsubramanian@users.noreply.github.com>
2025-07-14 21:48:52 +0530

    Time: 0 ms (100%), Space: 15.5 MB (92.39%)

---
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+    int n = nums.size();
+    int index = -1;
+    for(int i = n-2 ; i >= 0 ; --i){
+        if(nums[i]<nums[i+1]){
+            index = i; //find the dipt
+            break;
+        }
+
+    }
+    if(index!=-1){
+       int j = n-1;
+       while(nums[index] >= nums[j]) --j;
+       swap(nums[index],nums[j]);
+    }
+    reverse(nums.begin() + index + 1,nums.end());
+    }
+};
