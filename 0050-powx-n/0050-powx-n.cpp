a8ead35fee5025c6c472ffb82528989170d4b7db
Karthik Balasubramanian <karthikbalasubramanian08@gmail.com>
2025-08-29 18:16:09 +0530

    Time: 0 ms (100%), Space: 8.7 MB (13.92%)

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    double myPow(double x, long long n) {
+        if(n == 0) return 1.0;
+
+        if(n<0) return 1.0/myPow(x,-n);
+
+        double half = myPow(x,n/2);
+
+        if(n%2==0){
+            return half*half; 
+        }else{
+            return half*half*x;
+        }
+
+    }
+};
