8068164845bff6321c6349fd0ec3533ff7720ce5
Karthik Balasubramanian <karthikbalasubramanian08@gmail.com>
2025-09-26 13:31:51 +0530

    Time: 186 ms (51.73%), Space: 12.8 MB (39.51%)

---
+++ 0204-count-primes/0204-count-primes.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int countPrimes(int n) {
+        if(n<2) return 0;
+
+        vector<bool> bucket(n,true);
+        bucket[0]=bucket[1]=false;
+
+        for(int i=2; i*i<n; i++){
+            if(bucket[i]){
+                for(int j=i*i; j<n; j+=i){
+                    bucket[j] = false;
+                }
+            }
+        }
+        
+        int cnt = count(bucket.begin(),bucket.end(),true);
+        return cnt;
+    }
+};
