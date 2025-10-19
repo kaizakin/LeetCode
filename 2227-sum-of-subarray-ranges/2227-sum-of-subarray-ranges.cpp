ac9dcb90f497470b9c92962373dfe86b897622ff
Kaiza Kin <karthikbalasubramanian08@gmail.com>
2025-10-19 16:52:30 +0530

    Time: 7 ms (44.09%), Space: 15.8 MB (41.36%)

---
+++ 2227-sum-of-subarray-ranges/2227-sum-of-subarray-ranges.cpp
@@ -0,0 +1,83 @@
+class Solution {
+public:
+    long long sumSubarrayMins(vector<int>& arr) {
+        int n = arr.size();
+
+        vector<int> left(n), right(n);
+        stack<pair<int, int>> s1, s2;
+
+        // Find distance to previous smaller element
+        for (int i = 0; i < n; i++) {
+            int count = 1;
+            while (!s1.empty() && s1.top().first > arr[i]) {
+                count += s1.top().second;
+                s1.pop();
+            }
+            s1.push({arr[i], count});
+            left[i] = count;
+        }
+
+        // Find distance to next smaller element
+        for (int i = n - 1; i >= 0; i--) {
+            int count = 1;
+            while (!s2.empty() && s2.top().first >= arr[i]) {
+                count += s2.top().second;
+                s2.pop();
+            }
+            s2.push({arr[i], count});
+            right[i] = count;
+        }
+
+        long long ans = 0;
+        for (int i = 0; i < n; i++) {
+            ans = (ans + (long long)arr[i] * left[i] * right[i]);
+        }
+
+        return ans;
+    }
+
+
+    long long sumSubarrayMaxs(vector<int>& arr) {
+        int n = arr.size();
+
+        vector<int> left(n), right(n);
+        stack<pair<int, int>> s1, s2;
+
+        // Find distance to previous smaller element
+        for (int i = 0; i < n; i++) {
+            int count = 1;
+            while (!s1.empty() && s1.top().first < arr[i]) {
+                count += s1.top().second;
+                s1.pop();
+            }
+            s1.push({arr[i], count});
+            left[i] = count;
+        }
+
+        // Find distance to next smaller element
+        for (int i = n - 1; i >= 0; i--) {
+            int count = 1;
+            while (!s2.empty() && s2.top().first <= arr[i]) {
+                count += s2.top().second;
+                s2.pop();
+            }
+            s2.push({arr[i], count});
+            right[i] = count;
+        }
+
+        long long ans = 0;
+        for (int i = 0; i < n; i++) {
+            ans = (ans + (long long)arr[i] * left[i] * right[i]);
+        }
+
+        return ans;
+    }
+
+
+    long long subArrayRanges(vector<int>& nums) {
+        long long maxval = sumSubarrayMaxs(nums);
+        long long minval = sumSubarrayMins(nums);
+
+        return maxval - minval;
+    }
+};
