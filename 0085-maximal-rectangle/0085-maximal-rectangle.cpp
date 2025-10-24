2a25b6de564821835a7437b0b3db71957c98f2fa
Kaiza Kin <karthikbalasubramanian08@gmail.com>
2025-10-24 22:44:52 +0530

    Time: 3 ms (86.7%), Space: 19.3 MB (47.31%)

---
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -0,0 +1,58 @@
+class Solution {
+public:
+
+    int largestRectangleArea(vector<int>& heights) {
+        stack<int> st;
+        int maxArea = 0;
+        int n = heights.size();
+
+        for(int i=0; i<n; i++){
+            // if you found ia smaller element the the top element in the stack
+            while(!st.empty() && heights[st.top()] > heights[i]){
+                int index = st.top(); st.pop();
+                int pseindex = st.empty() ? -1 : st.top(); // the pse is the next element in the stack
+                int nseindex = i; // nse is current index
+                maxArea = max(heights[index] * (nseindex - pseindex - 1), maxArea); // next - prev -1
+            }
+            // we are pushing the index
+            st.push(i);
+        }
+
+        // if there are any leftouts in the stack
+        while(!st.empty()){
+            int index = st.top(); st.pop();
+            int nseindex = n; //coz these don't have a nse so the hypothetical last index
+            int pseindex = st.empty() ? -1 : st.top();
+            maxArea = max(heights[index] * (nseindex - pseindex - 1), maxArea);
+        }
+
+        return maxArea;
+    }
+
+    int maximalRectangle(vector<vector<char>>& matrix) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        int maxArea = 0;
+
+        vector<vector<int>> preSum(n,vector<int>(m));
+
+        for(int j=0; j<m; j++){
+            int sum = 0;
+            for(int i=0; i<n; i++){
+                sum += matrix[i][j] - '0';
+
+                if(matrix[i][j] == '0'){
+                    sum = 0;
+                }
+
+                preSum[i][j] = sum;
+            }
+        }
+
+        for(int i=0; i<n; i++){
+            maxArea = max(maxArea, largestRectangleArea(preSum[i]));
+        }
+
+        return maxArea;
+    }
+};
